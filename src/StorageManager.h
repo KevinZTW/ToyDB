c169720651b484854ad3d82cc5787ef3f14b4ddf
Kevin Zhang <kai821104@gmail.com>
2025-04-05 10:41:52 +0800

    feat: Add Storage Manager constructor

--- src/StorageManager.h
+++ src/StorageManager.h
@@ -5,11 +5,19 @@
 #ifndef TOY_DB_STORAGEMANAGER_H
 #define TOY_DB_STORAGEMANAGER_H
 
+#include <fstream>
+#include <filesystem>
+#include <iostream>
+
 namespace DB {
-    class StorageManager {
+class StorageManager {
+ public:
+  StorageManager(bool truncate_mode = true);
 
-    };
+ private:
+  const std::string database_filename = "toy_db.data";
+  std::fstream file_stream;
+};
 }
-
 
 #endif //TOY_DB_STORAGEMANAGER_H
